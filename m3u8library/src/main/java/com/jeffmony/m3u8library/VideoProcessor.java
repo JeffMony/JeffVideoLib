9eca11503eb31b5f96e42485adda557d1dfcda57
JeffMony <jeffmony@163.com>
2021-06-18 12:02:11 +0800

    提交最新的ffmpeg库
    Signed-off-by: JeffMony <jeffmony@163.com>

--- m3u8library/src/main/java/com/jeffmony/m3u8library/VideoProcessor.java
+++ m3u8library/src/main/java/com/jeffmony/m3u8library/VideoProcessor.java
@@ -1,8 +1,14 @@
 package com.jeffmony.m3u8library;
 
+import androidx.annotation.NonNull;
+
+import com.jeffmony.m3u8library.listener.IVideoTransformProgressListener;
+
 public class VideoProcessor {
 
     private static volatile boolean mIsLibLoaded = false;
+
+    private IVideoTransformProgressListener mListener;
 
     public static void loadLibrariesOnce() {
         synchronized (VideoProcessor.class) {
@@ -26,4 +32,18 @@ public class VideoProcessor {
     }
 
     public static native void initFFmpegOptions();
+
+    //转化视频的封装格式,M3U8 转化为 MP4格式
+    public native int transformVideo(String inputPath, String outputPath);
+
+    public void setOnVideoTransformProgressListener(@NonNull IVideoTransformProgressListener listener) {
+        mListener = listener;
+    }
+
+    //从native层调用上来,回调当前的视频转化进度
+    public void invokeVideoTransformProgress(float progress) {
+        if (mListener != null) {
+            mListener.onTransformProgress(progress);
+        }
+    }
 }
